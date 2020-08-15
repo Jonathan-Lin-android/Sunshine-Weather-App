e91f6d0c08fceac84c215ec287a563bd2fad32a7
Jonathan Lin <69509330+Jonathan-Lin-android@users.noreply.github.com>
2020-08-15 15:53:28 -0700

    Added sharing of weather data in detail activity

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.sunshine;
 
 import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -19,5 +22,20 @@ public class DetailActivity extends AppCompatActivity {
 
         TextView displayWeatherTV = findViewById(R.id.tv_display_weather);
         displayWeatherTV.setText(weatherStr);
+    }
+
+    private Intent createShareForecastIntent() {
+        return ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText(getIntent().getStringExtra(Intent.EXTRA_TEXT) + FORECAST_SHARE_HASHTAG)
+                .getIntent();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        menuItem.setIntent(createShareForecastIntent());
+        return true;
     }
 }
